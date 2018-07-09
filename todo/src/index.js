db2f37fb3eea3cffff7997da6813356bfaefdaf8
unknown <brcampbell001@outlook.com>
2018-07-09 16:30:38 -0500

    ToDo MVP Completed - Needs Styling

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {todoReducer} from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(todoReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
+ document.getElementById('root')
+);
